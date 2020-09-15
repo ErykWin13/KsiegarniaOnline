adbd0398abca06e2e05fb25089fd1ddf2e67969a
Eryk Winnicki <erykwin13@gmail.com>
2020-09-15 13:52:08 +0200

    Updates

---
+++ precache-manifest.6cc48c928696b948495a6e2a40c8d459.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc8e4ef162754d383d61dc6d81f15fe8",
+    "url": "/KsiegarniaOnline/index.html"
+  },
+  {
+    "revision": "690d514ac2e1dc9590dc",
+    "url": "/KsiegarniaOnline/static/css/main.326b3a83.chunk.css"
+  },
+  {
+    "revision": "2c33d7af828c1d7c2785",
+    "url": "/KsiegarniaOnline/static/js/2.7bc7f2ac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/KsiegarniaOnline/static/js/2.7bc7f2ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "690d514ac2e1dc9590dc",
+    "url": "/KsiegarniaOnline/static/js/main.d876aed6.chunk.js"
+  },
+  {
+    "revision": "02389696aa585354e74f",
+    "url": "/KsiegarniaOnline/static/js/runtime-main.ffbbe8aa.js"
+  },
+  {
+    "revision": "472ddf539da73127705d2d8864f64a89",
+    "url": "/KsiegarniaOnline/static/media/12-życiowych-zasad.472ddf53.jpg"
+  },
+  {
+    "revision": "3b4eb72d0c01d2faadca051724a0c5db",
+    "url": "/KsiegarniaOnline/static/media/bazar-złych-snów.3b4eb72d.jpg"
+  },
+  {
+    "revision": "201ce8f4e3aea0adc9a7635b20b10295",
+    "url": "/KsiegarniaOnline/static/media/był-sobie-pies-2.201ce8f4.jpg"
+  },
+  {
+    "revision": "3047b13e2b20b5fb40a6593fff59a928",
+    "url": "/KsiegarniaOnline/static/media/był-sobie-pies.3047b13e.jpg"
+  },
+  {
+    "revision": "fdd90d95a8350ee00bab5b2a060886d3",
+    "url": "/KsiegarniaOnline/static/media/hobbit.fdd90d95.jpg"
+  },
+  {
+    "revision": "81725db239f471e5727c7e4ba707807e",
+    "url": "/KsiegarniaOnline/static/media/i-że-cię-nie-opuszczę.81725db2.jpg"
+  },
+  {
+    "revision": "5da5fe3ffdb749ac26f9c934697cc4f6",
+    "url": "/KsiegarniaOnline/static/media/już-mnie-nie-oszukasz.5da5fe3f.jpg"
+  },
+  {
+    "revision": "b8d45f8a2ecd8b655863f68d350ab536",
+    "url": "/KsiegarniaOnline/static/media/narzędzia-tytanów.b8d45f8a.jpg"
+  },
+  {
+    "revision": "12e80993313fa3f9e4750b0cca8fc956",
+    "url": "/KsiegarniaOnline/static/media/nocna-runda.12e80993.jpg"
+  },
+  {
+    "revision": "45568132259f20884cf1c3325b64ae08",
+    "url": "/KsiegarniaOnline/static/media/obudź-w-sobie-olbrzyma.45568132.jpg"
+  },
+  {
+    "revision": "c7b47e7f6daf009e3f41f53b83dd7589",
+    "url": "/KsiegarniaOnline/static/media/odbiorę-ci-wszystko.c7b47e7f.jpg"
+  },
+  {
+    "revision": "4b5e434abb06c68391b0f37e052746f9",
+    "url": "/KsiegarniaOnline/static/media/potęga-pewności-siebie.4b5e434a.jpg"
+  },
+  {
+    "revision": "a03cdb30eee5f81795d9370738132e10",
+    "url": "/KsiegarniaOnline/static/media/smętarz-dla-zwierzaków.a03cdb30.jpg"
+  },
+  {
+    "revision": "ba033dfb7d4ab0d488060accebbab057",
+    "url": "/KsiegarniaOnline/static/media/to.ba033dfb.jpg"
+  },
+  {
+    "revision": "8a11e5be78b5832af289498781c88d97",
+    "url": "/KsiegarniaOnline/static/media/w-pustyni-i-w-puszczy.8a11e5be.jpg"
+  },
+  {
+    "revision": "1c3cf1b0370ad54a5117c0b9900b3192",
+    "url": "/KsiegarniaOnline/static/media/wiedźmin-krew-elfów.1c3cf1b0.jpg"
+  },
+  {
+    "revision": "c7b584a7c445243f7e90cc3c5d1d846d",
+    "url": "/KsiegarniaOnline/static/media/wiedźmin-ostatnie-życzenie.c7b584a7.jpg"
+  },
+  {
+    "revision": "fd4fead6710daf0f45a3f0b70cfc5e9d",
+    "url": "/KsiegarniaOnline/static/media/wiedźmin-pani-jeziora.fd4fead6.jpg"
+  },
+  {
+    "revision": "a8d2b663736f89fdeedbc877b6f7ab73",
+    "url": "/KsiegarniaOnline/static/media/wyspa-skarbów.a8d2b663.jpg"
+  },
+  {
+    "revision": "ddc174f958f8ec6c5e3b32c6cdec319c",
+    "url": "/KsiegarniaOnline/static/media/łowca-snów.ddc174f9.jpg"
+  }
+]);
